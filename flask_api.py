78c3aa5d548cb164d9f380f8233d6dd140d119bd
vvsnmurthy28 <87513676+vvsnmurthy28@users.noreply.github.com>
2023-12-27 11:39:57 +0530

    add flask_api.py file

---
+++ flask_api.py
@@ -0,0 +1,192 @@
+from flask import Flask, jsonify, request
+from elasticsearch import Elasticsearch
+import json
+
+app = Flask(__name__)
+es = Elasticsearch(["http://localhost:9200"])
+
+@app.route('/get_doc/<index_name>/<doc_id>', methods=['GET'])
+@app.route('/get_doc/<index_name>/', methods=['GET'])
+def get_document(index_name, doc_id=None):
+    try:
+        if doc_id is not None:
+            if es.exists(index=index_name, id=doc_id):
+                result = es.get(index=index_name, id=doc_id)
+                document = result['_source']
+                return jsonify(document), 200
+            else:
+                return jsonify({"error": f"Doc ID '{doc_id}' does not exist in index '{index_name}'"}), 404
+        else:
+            result = es.search(index=index_name, body={"query": {"match_all": {}}})
+            documents = [hit['_source'] for hit in result['hits']['hits']]
+            return jsonify(documents), 200
+    except Exception as e:
+        error_message = {'error': str(e)}
+        return jsonify(error_message), 500
+
+
+@app.route('/get_by_query/<index_name>', methods=['GET'])
+def get_by_query(index_name):
+    try:
+        search_query = request.get_json()
+        if not search_query:
+            return jsonify({"error": "query input required"}), 400
+        result = dict(es.search(index=index_name, body=search_query))
+        documents = [hit['_source'] for hit in result['hits']['hits']]
+        return jsonify({"documents": documents}), 200
+    except Exception as e:
+        error_message = {'error': str(e)}
+        return jsonify(error_message), 500
+
+
+#=================================================================================================================
+
+@app.route('/ingest_doc/<index_name>', methods=['POST'])
+def ingest_document(index_name):
+    try:
+        data = request.get_json()
+        res = dict(es.index(index=index_name, body=data))
+        if res['result'] == 'created':
+            return jsonify({"message": "Document successfully ingested", "es_response": res}), 200
+        else:
+            return jsonify({"error": "Failed to ingest document", "es_response": res}), 500
+    except Exception as e:
+        error_message = {'error': str(e)}
+        return jsonify(error_message), 500  # Use 500 status code for server errors
+
+
+@app.route('/ingest_bulk/<index_name>', methods=['POST'])
+def ingest_bulk_data(index_name):
+    try:
+        # Check if the request contains a file with the key 'file'
+        print(request.files) #ImmutableMultiDict([('file', <FileStorage: 'data.json' ('application/json')>)])
+        if 'file' not in request.files:
+            return jsonify({"error": "No file provided"}), 400
+        file = request.files['file']
+        # Check if the file has an allowed extension (e.g., .json)
+        allowed_extensions = {'json'}
+        print(file) # <FileStorage: 'data.json' ('application/json')>
+        print(file.filename) # data.json
+        print(file.filename.rsplit(".",1))
+        if '.' in file.filename and file.filename.rsplit('.', 1)[1].lower() not in allowed_extensions:
+            return jsonify({"error": "Invalid file extension. Allowed extensions: .json"}), 400
+        # Load JSON data from the file
+        try:
+            data = json.load(file)
+        except json.JSONDecodeError as e:
+            return jsonify({"error": f"Invalid JSON format in the file: {str(e)}"}), 400
+        # Ingesting bulk data into Elasticsearch
+        success_count = 0
+        failure_count = 0
+        for doc in data:
+            res = es.index(index=index_name, body=doc)
+            if res['result'] == 'created':
+                success_count += 1
+            else:
+                failure_count += 1
+        return jsonify({"successfully uploaded documents": success_count, "total_documents": len(data), "failed Documents " : failure_count }), 200
+    except Exception as e:
+        error_message = {'error': str(e)}
+        return jsonify(error_message), 500
+
+
+#=================================================================================================================
+
+#deletion with document ID
+@app.route('/delete_doc/<index_name>/<doc_id>', methods=['DELETE'])
+def delete_document(index_name, doc_id):
+    try:
+        if es.exists(index=index_name, id=doc_id):
+            res = dict(es.delete(index=index_name, id=doc_id))
+            if res['result'] == 'deleted' :
+                return jsonify({
+                    "message" : res,
+                    "document_name" : doc_id,
+                    "index_name" : index_name,
+                    "deletion" : "successful"
+                }),200
+            else:
+                return jsonify({
+                    "message" : res,
+                    "index" : index_name,
+                    "deletion" : "unsuccessful"
+                }),500
+        else:
+            return jsonify({"error": f"Document with ID '{doc_id}' does not exist in index '{index_name}'"}), 404
+    except Exception as e:
+        error_message = {'error': str(e)}
+        return jsonify(error_message), 500
+
+
+@app.route('/delete_by_query/<index_name>', methods=['DELETE'])
+def delete_by_query(index_name):
+    try:
+        query = request.get_json()
+        if not query:
+            return jsonify({"error": "Query parameter is required"}), 400
+        response = dict(es.delete_by_query(index=index_name, body=query))
+        print(response)
+        
+        """
+        {'took': 24, 'timed_out': False, 'total': 14, 'deleted': 14, 'batches': 1, 
+        'version_conflicts': 0, 'noops': 0, 'retries': {'bulk': 0, 'search': 0}, 
+        'throttled_millis': 0, 'requests_per_second': -1.0, 'throttled_until_millis': 0, 
+        'failures': []}
+        """
+        if response['deleted']:
+            return jsonify({
+                "message": f"Documents matching the query successfully deleted from index '{index_name}'",
+                "deleted": response['deleted'],
+                "took": response['took'],
+                'total_match': response['total']
+            }), 200
+        else:
+            return jsonify({
+                "message": "No matching documents found to delete in index",
+                "error_message": response
+            }), 500
+    except Exception as e:
+        error_message = {'error': str(e)}
+        return jsonify(error_message), 500
+
+
+#=================================================================================================================
+
+@app.route('/update_doc/<index_name>/<doc_id>', methods=['POST'])
+@app.route('/update_by_query/<index_name>', methods=['POST'])
+def update_by_query(index_name,doc_id=None):
+    try:
+        if doc_id:
+            if es.exists(index=index_name, id=doc_id):
+                update_data = request.get_json()
+                if not update_data:
+                    return jsonify({"error": "Update data parameter is required"}), 400
+                response = es.update(index=index_name, id=doc_id, body={"doc": update_data})
+                if 'result' in response and response['result'] == 'updated':
+                    return jsonify({"message": f"Document with ID '{doc_id}' successfully updated in index '{index_name}'"}), 200
+                else:
+                    return jsonify({"error": f"Failed to update document with ID '{doc_id}' in index '{index_name}'"}), 500
+            else:
+                return jsonify({"error": f"Document with ID '{doc_id}' does not exist in index '{index_name}'"}), 404
+        else:
+            update_query = request.get_json()
+            print(update_query)
+            if not update_query:
+                return jsonify({"error": "Update query parameter is required"}), 400
+            response = dict(es.update_by_query(index=index_name, body=update_query))
+            print(response)
+            if response['updated']:
+                return jsonify({
+                    "Result": f"Documents matching the query successfully updated in index '{index_name}'",
+                    "Response Body": response
+                }), 200
+            else:
+                return jsonify({"error": "Failed to perform update by query"}), 500
+    except Exception as e:
+        error_message = {'error': str(e)}
+        return jsonify(error_message), 500
+
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
